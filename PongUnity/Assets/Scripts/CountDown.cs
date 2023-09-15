cf46c97f88545af645f81d5a3bb8d5752eb5b50c
WUStudent <wustudent@webster.edu>
2023-09-15 15:43:15 -0500

    Count Down Timer

---
+++ PongUnity/Assets/Scripts/CountDown.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CountDown : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    public TMP_Text countdownText;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");    
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
